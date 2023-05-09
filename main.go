02c78e4541dd5fb9dfc9310df2ff131df794ba27
kldzj <github@kldzj.dev>
2023-05-09 02:24:21 +0200

    chore: refactor main.go

--- main.go
+++ main.go
@@ -17,33 +17,35 @@ func main() {
 pzmod --file server.ini get list
 pzmod --file server.ini get name
 pzmod --file server.ini set name "My Server"`,
-		Run: interactive.Execute,
-		PreRun: func(cmd *cobra.Command, args []string) {
-			if !version.IsSet() {
-				return
-			}
-
-			updater, err := version.NewUpdater()
-			if err != nil {
-				return
-			}
-
-			ver := version.Get()
-			latest, err := version.GetLatestRelease(updater)
-			if err != nil {
-				return
-			}
-
-			if version.IsLatest(ver, latest) {
-				return
-			}
-
-			cmd.Println(util.Info, "A new version of pzmod is available:", latest.Version())
-			cmd.Println(util.Info, "Run `pzmod update` to update to the latest version.")
-		},
+		PreRun: checkForUpdate,
+		Run:    interactive.Execute,
 	}
 
 	commands.SetFileFlag(rootCmd)
 	commands.Init(rootCmd)
 	rootCmd.Execute()
+}
+
+func checkForUpdate(cmd *cobra.Command, args []string) {
+	if !version.IsSet() {
+		return
+	}
+
+	updater, err := version.NewUpdater()
+	if err != nil {
+		return
+	}
+
+	ver := version.Get()
+	latest, err := version.GetLatestRelease(updater)
+	if err != nil {
+		return
+	}
+
+	if version.IsLatest(ver, latest) {
+		return
+	}
+
+	cmd.Println(util.Info, "A new version of pzmod is available:", latest.Version())
+	cmd.Println(util.Info, "Run `pzmod update` to update to the latest version.")
 }
